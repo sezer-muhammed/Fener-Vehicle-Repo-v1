c6ad77d1b558da9a0bebeb925707180922140380
muhammed-sezer <e230531@metu.edu.tr>
2021-12-13 22:15:27 +0300

    Some Constants added

--- src/fener_package/fener_package/fener_arduino_comm.py
+++ src/fener_package/fener_package/fener_arduino_comm.py
@@ -7,6 +7,10 @@ import time
 import serial
 from os import system
 
+KEY__ANGLE_MULTIPLIER = 400
+KEY_SPEED_MULTIPLIER = 30
+KEY_ANGLE_BNO_MULTIPLIER = 25
+
 class MyNode(Node):
     def __init__(self):
         super().__init__("arduino_comm")
@@ -33,28 +37,26 @@ class MyNode(Node):
 
     def timer_callback(self):
         
-        self.keyboard_packet[0:3] = [self.key_speed, min(3000, max(0, self.key_angle + 25 * (self.aim_angle - self.bno055_heading))), self.bno055_heading%360]
+        self.keyboard_packet[0:3] = [self.key_speed, min(3000, max(0, self.key_angle + KEY_ANGLE_BNO_MULTIPLIER * (self.aim_angle - self.bno055_heading))), self.bno055_heading%360]
 
         self.encoder = self.send_to_arduino(self.keyboard_commands, self.keyboard_packet)
-        data = self.ard.readline().decode()
-#        print(data)
-#        print(self.keyboard_packet)
+        debug_data = self.ard.readline().decode()
 
 
     def send_to_arduino(self, commands, values):
 
             msg = bytearray()
-
             for command, value in zip(commands, values):
                     first_byte = value % 256
                     second_byte = value // 256
                     command_byte = command * 32
                     msg.append(first_byte)
                     msg.append(second_byte + command_byte)
+
             self.ard.write(msg)
 
             output = np.array(list(self.ard.read(size = 4))) - 1
-            #print(output)
+
             return output
 
     def keyboard_callback(self, msg):
@@ -64,16 +66,11 @@ class MyNode(Node):
         
         if (keys[1] - keys[3]) != 0:
             self.aim_angle = self.bno055_heading
-        self.key_speed = int(self.speed_mul * 30 * keys[0] * (1 - keys[2]))
-        self.key_angle = int(1500 - (keys[1] - keys[3]) * 400) # mid is 1500
-        #self.encoder = self.send_to_arduino(self.keyboard_commands, self.keyboard_packet)
-        #data = self.ard.readline() 
-
-        #print(self.keyboard_packet)
+        self.key_speed = int(self.speed_mul * KEY_SPEED_MULTIPLIER * keys[0] * (1 - keys[2]))
+        self.key_angle = int(1500 - (keys[1] - keys[3]) * KEY__ANGLE_MULTIPLIER) # mid is 1500
 
     def bno055_callback(self, msg):
         self.bno055_heading = int(msg.data[3])
-        #print(self.bno055_heading)
 
 
 def main(args=None):
--- src/fener_package/fener_package/fener_solo_cam_pub_node.py
+++ src/fener_package/fener_package/fener_solo_cam_pub_node.py
@@ -8,6 +8,8 @@ import numpy as np
 from sensor_msgs.msg import CompressedImage, Image
 from cv_bridge import CvBridge
 
+RATIO = 0.35
+
 def resize(img, resize_factor):
         resized_img = jetson.utils.cudaAllocMapped(width=img.width * resize_factor[0], height=img.height * resize_factor[1], format=img.format)
         jetson.utils.cudaResize(img, resized_img)
@@ -24,7 +26,7 @@ def main():
 
     while True:
         img = camera.Capture()
-        img = resize(img, (0.35, 0.35))
+        img = resize(img, (RATIO, RATIO))
         np_img = jetson.utils.cudaToNumpy(img)
         jetson.utils.cudaDeviceSynchronize()
         msg = br.cv2_to_compressed_imgmsg(np_img)
